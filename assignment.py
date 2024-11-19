e048027d36a2f50d1d5aad2248d2e409f50d8a75
Sarah Rizvi <sarah.rizvi@stu.ocsb.ca>
2024-11-19 15:33:03 +0000

    Finished code for Assignemnt 5

--- assignment.py
+++ assignment.py
@@ -1,6 +1,17 @@
-def calculatePi(digits):
-  result = 0
-  return result
+def calculate_pi(iterations):
+    pi_approx = 0
+    for n in range(iterations):
+        pi_approx += ((-1) ** n) / (2 * n + 1)
+    pi_approx *= 4
+    return pi_approx
 
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+# Get the number of iterations from the user
+try:
+    iterations = int(input("Enter the number of iterations: "))
+    if iterations <= 0:
+        print("Please enter a positive number.")
+    else:
+        pi_value = calculate_pi(iterations)
+        print(f"Approximation of PI after {iterations} iterations: {pi_value}")
+except ValueError:
+    print("Please enter a valid integer.")
